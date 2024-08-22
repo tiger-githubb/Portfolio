3df5d237d61e6958712868d4728fa384d82df4ad
KARBOU Aristide <aristidetiger12@gmail.com>
2024-08-22 09:16:18 +0100

    instalation of shad cn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h3>hello </h3> <Button variant="destructive">destructive</Button>
+    </div>
+  );
 }
